b55b99a334eda5bd43a326ed6c8be35cc141fa3b
templedomain <holycat@ctemplar.com>
2022-01-24 16:58:57 +0100

    added error msg for password

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -18,9 +18,16 @@ export class RegisterComponent implements OnInit {
   }
 
   register(){
-    console.log(this.model)
+    let regExplarge = /[A-Z]/;
+    let regExpnumb = /[0-9]/
+
+
     if (this.model.password != this.model.confirm_password){
       this.errorMsg = "Confirm Password does not match with Password."
+    } else if ( !regExplarge.test(this.model.password) || !regExpnumb.test(this.model.password) ){
+      this.errorMsg = "Password must contain at least one upper case letter and at least one number."
+    } else if (this.model.password.length < 4){
+      this.errorMsg = "Password must must be at least 4 characters long."
     } else {
       this.accountService.register(this.model).subscribe(res => {
         this.router.navigateByUrl("login");
